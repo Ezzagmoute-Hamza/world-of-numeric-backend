a149eeab4e3d10212024ea084db13bd5e69c4ff2
Ezzagmoute Hamza <ezzagmoute.hamza@gmail.com>
2024-12-03 17:56:22 +0100

    enhanced the swagger api documentation

--- config/swagger.ts
+++ config/swagger.ts
@@ -3,13 +3,13 @@ export default {
     "swagger": "2.0",
     "info": {
         "version": "1.0.0",
-        "title": "Analytics Store Tool API",
-        "description": "Analytics Store Tool Backend to allow store administrator to analyse his store in a seamless way"
+        "title": "Analytics Store Tool API Documentation",
+        "description": "Analytics Store Tool That allows the store administrator to analyse his store in a seamless way"
     },
     "host": process.env.BASE_URL?.replace("http://", "").replace("https://", ""),
     "basePath": "/",
     "schemes": [
-        "http"
+        "http","https"
     ],
    "paths":{
         "/products": {
@@ -43,23 +43,26 @@ export default {
             }
         },
         "/analytics/total_sales": {
-            "get": {
-                "description": "Retourne le montant total des ventes pour la période sélectionnée.",
-                "parameters":{
-                    "in": "query",
-                    "name": "period",
-                    "required": false,
-                    "schema":{
-                        "type": "string",
-                       "enum": ["7d", "30d", "12m"]
-                    }  
+        "get": {
+            "description": "Retourne le montant total des ventes pour la période sélectionnée.",
+            "parameters": [
+            {
+                "name": "period",
+                "in": "query",
+                "required": false,
+                "schema": {
+                "type": "string",
+                "enum": ["7d", "30d", "12m"]
                 },
-                "responses": {
-                    "200": {
-                        "description": "OK"
-                    }
-                }
+                "description": "Spécifiez la période pour le montant total des ventes. Exemple : '7d' pour les 7 derniers jours, '30d' pour les 30 derniers jours, '12m' pour les 12 derniers mois."
             }
+            ],
+            "responses": {
+            "200": {
+                "description": "Retourne le montant total des ventes pour la période sélectionnée."
+            }
+            }
+        }
         }
    }
 }
